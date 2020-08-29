2f2f98f209ba5434a544e36a37abc6ecd73e3898
Navid-S-B <navid.bhuiyan45@outlook.com.au>
2020-08-29 22:21:01 +1000

    new filename

--- job_notifier.py
+++ job_notifier.py
@@ -12,16 +12,19 @@ This was integrated into a Rasperry Pi.
 import requests
 from bs4 import BeautifulSoup
 
-
+# Create file for woolworths jobs. It only looks on the first page.
 def woolworths():
-
-    response = requests.get("https://www.wowcareers.com.au/jobs/listing?query=&refine_requisition_number=&brand=&state=NSW&country=Australia&refine_posted_within=&expertise=&worktype=&postcode=&location_within=&role=")
+    
+    url = "https://www.wowcareers.com.au/jobs/listing?query=&refine_requisition_number=&brand=&state=NSW&country=Australia&refine_posted_within=&expertise=&worktype=&postcode=&location_within=&role="
+    response = requests.get(url)
     soup = BeautifulSoup(response.text,'html.parser')
     find_jobs = soup.find_all('h3', class_ = 'jobs__listing-item-title')
     find_dates = soup.find_all('span', class_ = "jobs__listing-item-icon--calendar")
     find_job_type = soup.find_all('span', class_ = "jobs__listing-item-icon--clock")
 
-    job_file = open("woolworths_jobs.txt", 'w')
+    job_file = open("woolworths_jobs_report.txt", 'w')
+
+    job_file.write("{}\n\n".format(url))
 
     for i in range(len(find_jobs)):
         job_file.write("{} | {} | {}\n".format(find_jobs[i].a.text, find_job_type[i].text, find_dates[i].text))
@@ -30,7 +33,7 @@ def woolworths():
 
     return 0
     
-
-woolworths()
+if __name__ == "__main__":
+    woolworths()
 
 #<div class="jobs__listing-item-brand">Fuel</div>
