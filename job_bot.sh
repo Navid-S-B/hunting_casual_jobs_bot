79ffc4fe6cb2675c2324f0d078b1391f89196f18
Navid-S-B <navid.bhuiyan45@outlook.com.au>
2020-08-30 21:29:18 +1000

    Added error logging and checking measures

--- job_bot.sh
+++ job_bot.sh
@@ -3,10 +3,20 @@
 # Keep bot active to scan every 2 days
 while true
 do
-    python3 job_report_generator.py
-    rm *report.txt
-    # Activate python script every 2 days
+    # Activate program every 2 days
+    sleep 2d
+    output="`python3 job_report_generator.py`"
     date="`date`"
-    echo "Report Sent on $date"
-    sleep 3d
+    # Error detection measures;
+    # the python script will produce an error or success message
+    # and log it.
+    echo "$output | $date\n" >> job_report_generator_log.txt
+    if test "$output" = "Reports Sent"
+    then
+        rm *report.txt
+    else
+        # Remove files if any were produced
+        rm *report.txt 2> /dev/null
+        exit 1
+    fi
 done
