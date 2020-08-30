248495a0b691348388b42751825a26cac21f88fc
Navid-S-B <navid.bhuiyan45@outlook.com.au>
2020-08-30 16:04:06 +1000

    Updated comments

--- job_bot.sh
+++ job_bot.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-# Keep bot active to scan every 3 days
+# Keep bot active to scan every 2 days
 while true
 do
     python3 job_report_generator.py
     rm *report.txt
-    # Activate every 2 days
+    # Activate python script every 2 days
     date="`date`"
     echo "Report Sent on $date"
     sleep 3d
